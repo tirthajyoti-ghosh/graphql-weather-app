f2d5b32c1fa1374865294cd9396354445297c09d
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-10-09 23:26:05 +0530

    Integrate graphql query in Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,35 @@
 import React, { useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
+import { GET_WEATHER_QUERY } from '../graphql/queries';
 
 const Home = () => {
     const [city, setCity] = useState('');
+
+    const [getWeather, { loading, data, error }] = useLazyQuery(GET_WEATHER_QUERY, {
+        variables: {
+            name: city,
+        },
+    });
+
+    if (error) {
+        return <h1>Error found</h1>;
+    }
+
+    if (loading) {
+        return <h1>Loading....</h1>;
+    }
+
+    if (data) {
+        console.log(data);
+        const countryName = new Intl.DisplayNames(['en'], { type: 'region' });
+        console.log(countryName.of(data.getCityByName.country));
+    }
 
     return (
         <div className="home">
             <h1>Search for weather by city</h1>
             <input type="text" placeholder="City..." value={city} onChange={(event) => setCity(event.target.value)} />
-            <button type="button">Search</button>
+            <button type="button" onClick={() => getWeather()}>Search</button>
         </div>
     );
 };

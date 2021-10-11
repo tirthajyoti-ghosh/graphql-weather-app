9fe8eee92c8ff3a60fa408be51625abf9db3ca3e
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-10-11 20:41:55 +0530

    Add fetching bg image functionality in Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,29 @@
-import React, { useState } from 'react';
+/* eslint-disable max-len */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useLazyQuery } from '@apollo/client';
+
 import { GET_WEATHER_QUERY } from '../graphql/queries';
 
 const Home = () => {
     const [city, setCity] = useState('');
+    const [backgroundImage, setBackgroundImage] = useState('');
 
     const [getWeather, { loading, data, error }] = useLazyQuery(GET_WEATHER_QUERY);
+
+    // Get weather image ONLY when `data` changes.
+    useEffect(() => {
+        async function fetchBackgroundImage() {
+            // Since passing a dependency array will cause the function to run on mount, this if condition prevents that. As `data` is undefined on mount.
+            if (data && data.getCityByName) {
+                const { data: images } = await axios.get(`https://utility-endpoints.netlify.app/.netlify/functions/unsplash-image-search?orientation=landscape&query=${data.getCityByName.name} ${data.getCityByName.weather.summary.description}`);
+
+                setBackgroundImage(images.results[Math.floor(Math.random() * images.results.length)].urls.full);
+            }
+        }
+
+        fetchBackgroundImage();
+    }, [data]);
 
     if (error) {
         return <h1>Error found</h1>;
@@ -21,10 +39,15 @@ const Home = () => {
         <div className="home">
             <h1>Search for weather by city</h1>
             <input type="text" placeholder="City..." value={city} onChange={(event) => setCity(event.target.value)} />
-            <button type="button" onClick={() => getWeather({ variables: { name: city } })}>Search</button>
+            <button
+                type="button"
+                onClick={() => getWeather({ variables: { name: city } })}
+            >
+                Search
+            </button>
 
-            {data && data.getCityByName && (
-                <>
+            {data && data.getCityByName && backgroundImage && (
+                <div style={{ background: `url(${backgroundImage})` }}>
                     <h3>
                         Name:
                         {' '}
@@ -60,7 +83,7 @@ const Home = () => {
                         {' '}
                         {data.getCityByName.weather.clouds.all}
                     </h3>
-                </>
+                </div>
             )}
         </div>
     );
